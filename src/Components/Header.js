205887f501b0ece969f7962dcaf1fc32221d0cd8
xinrong789 <xinronglee@126.com>
2025-04-07 03:39:36 +0930

    change linkedin

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -6,7 +6,7 @@ class Header extends Component {
   render() {
     if (!this.props.data) return null
 
-    const project = this.props.data.project
+    const linkedin = this.props.data.linkedin
     const github = this.props.data.github
     const name = this.props.data.name
     const description = this.props.data.description
@@ -61,8 +61,8 @@ class Header extends Component {
             <hr />
             <Fade bottom duration={2000}>
               <ul className="social">
-                <a href={project} className="button btn project-btn">
-                  <i className="fa fa-book"></i>Project
+                <a href={linkedin} className="button btn project-btn">
+                  <i className="fa fa-book"></i>LinkedIn
                 </a>
                 <a href={github} className="button btn github-btn">
                   <i className="fa fa-github"></i>Github
